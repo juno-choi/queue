6ffd404ae68cda1f6608c7ffbd45db00e5bffa61
juno-choi <ililil9482@naver.com>
2026-02-19 13:29:13 +0900

    fix: 단일 이벤트 등록

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -11,42 +11,7 @@ awslocal sns create-topic --name main-topic --region $REGION
 
 # ========== SQS Queues ==========
 echo "Creating SQS queues..."
-awslocal sqs create-queue --queue-name user-service --region $REGION
-awslocal sqs create-queue --queue-name admin-service --region $REGION
 awslocal sqs create-queue --queue-name core-service --region $REGION
-
-# ========== user-service 구독 (deposit, join) ==========
-echo "Subscribing user-service..."
-
-USER_SUB_ARN=$(awslocal sns subscribe \
-  --topic-arn $TOPIC_ARN \
-  --protocol sqs \
-  --notification-endpoint "arn:aws:sqs:${REGION}:${ACCOUNT_ID}:user-service" \
-  --region $REGION \
-  --output text --query 'SubscriptionArn')
-
-awslocal sns set-subscription-attributes \
-  --subscription-arn $USER_SUB_ARN \
-  --attribute-name RawMessageDelivery \
-  --attribute-value true \
-  --region $REGION
-
-
-# ========== admin-service 구독 (deposit, join) ==========
-echo "Subscribing admin-service..."
-ADMIN_SUB_ARN=$(awslocal sns subscribe \
-  --topic-arn $TOPIC_ARN \
-  --protocol sqs \
-  --notification-endpoint "arn:aws:sqs:${REGION}:${ACCOUNT_ID}:admin-service" \
-  --region $REGION \
-  --output text --query 'SubscriptionArn')
-
-awslocal sns set-subscription-attributes \
-  --subscription-arn $ADMIN_SUB_ARN \
-  --attribute-name RawMessageDelivery \
-  --attribute-value true \
-  --region $REGION
-
 
 # ========== core-service 구독 (deposit, mail) ==========
 echo "Subscribing core-service..."
